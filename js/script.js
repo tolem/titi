73358bb24b81561f406e146518438ab071fcb2e9
lami <olamide2olowoniyi@gmail.com>
2019-05-16 01:24:54 +0100

    update

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Data Scientist.", "Enteprenuer.", "Focused.," "Software Engineer"],
+		strings: ["Data Scientist.", "Enteprenuer.", "Focused.,", "Software Engineer"],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
